47044f08f8ec7422a2c825164979228445c234b5
Ronald Escobar <ronaldescobarj@gmail.com>
2020-11-23 08:24:28 -0400

    first commit

---
+++ Main.java
@@ -0,0 +1,30 @@
+package com.test;
+
+public class TestClass extends SuperClass {
+
+  public String newVar = "my var";
+  public int myVar = 9;
+  public long longVar = 123456789;
+  private String newString = "new string";
+  private boolean newBoolean = true;
+
+  private void method()
+  {
+
+  }
+
+  public boolean isChecked = true;
+
+  public TestClass()
+  {
+    int varUpd = 100;
+    System.out.println("hello world");
+  }
+
+  public int sum(int a, int b) {
+      return a + b;
+  }
+
+  public int counter = 5;
+
+}
